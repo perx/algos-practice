661f65ac0afcb3386a928a08f5ce2e9f06b2ac0c
perx <jaspreet.hd@gmail.com>
2018-06-01 14:49:44 +0530

    completed multiply

--- karatsubaLongNumberMultiplication.js
+++ karatsubaLongNumberMultiplication.js
@@ -1,4 +1,4 @@
-var multiply=function(a,b){
+var karatsuba=function(a,b){
 	a=a+"";
 	b=b+"";
 	
@@ -7,20 +7,48 @@ var multiply=function(a,b){
 	a.padStart(Math.max(a.length,b.length),"0");
 	b.padStart(Math.max(a.length,b.length),"0");
 	
-	let h=Math.ceil(a.length/2);
-	let x1=a.substring(0,h);
-	let x2=a.substring(h+1);
-	let y1=a.substring(0,h);
-	let y2=a.substring(h+1);
-	
-	let res1=multiply(x1,y1);
-	let res2=multiply(x2,y2);
-	let res3=multiply(parseInt(x1+x2),parseInt(y1+y2))-res1-res2;
-	res1=res1.padEnd(a.length,"0");
-	res3=res3.padEnd(h,"0");
-	return res1+res2+res3;
+	return multiply([...a],[...b]).join('');
 };
-
+var multiply=function(arr1,arr2){
+	if(arr1.length==1 && arr2.length ==1)
+		return [arr1[0]*arr2[0]];
+	
+	//padding the arrays
+	while(arr1.length>arr2.length)
+		arr2.unshift(0);
+	while(arr1.length<arr2.length)
+		arr1.unshift(0);	
+	
+	//carry out multiplication
+	let ans=[];
+	let h=Math.floor(arr1.length/2),factor=Math.ceil(arr1.length/2);
+	let a=arr1.slice(0,h),b=arr1.slice(h);
+	let c=arr2.slice(0,h),d=arr2.slice(h);
+	console.log("half is",h);
+	let res1=multiply(a,c);
+	let res2=multiply(b,d);
+	let res3=multiply(add(a,b),add(c,d));
+	let res4=add(res1,res2);
+	let term3=subtract(res3,res4);
+	console.log("before padding",res1,term3,res2);
+	
+	for(let j=0;j<2*factor;j++){
+		res1.push(0);
+	}
+	
+	
+	for(let j=0;j<factor;j++){
+		term3.push(0);	
+	}
+	console.log("after padding",res1,term3,res2);
+	ans=add(res1,add(res2,term3));
+	//remove padding
+	while(ans[0]==0)
+	{
+		ans.shift();
+	}
+	return ans;
+};
 var add=function(arr1,arr2){
 	while(arr1.length>arr2.length)
 		arr2.unshift(0);
@@ -89,3 +117,5 @@ var subtract=function(arr1,arr2){
 
 exports.add=add;
 exports.sub=subtract;
+exports.multiply=multiply;
+exports.karatsuba=karatsuba;
